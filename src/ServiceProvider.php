6d80dbb87eb8f0da3fd9dd4b4fb1f54599f2091a
Yuriy Potemkin <ysoft2000@mail.ru>
2023-01-15 11:52:21 +0300

    add config publish

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -7,6 +7,11 @@ use Sentry\ClientBuilderInterface;
 
 class ServiceProvider extends BaseServiceProvider
 {
+    public function boot()
+    {
+        $this->registerPublishes();
+    }
+    
     public function register()
     {
         $this->mergeConfigFrom(__DIR__.'/../config/sentry_queue.php', 'sentry_queue');
@@ -14,5 +19,16 @@ class ServiceProvider extends BaseServiceProvider
         $this->app->resolving(ClientBuilderInterface::class, function (ClientBuilderInterface $clientBuilder) {
             $clientBuilder->setTransportFactory(new TransportFactory());
         });
+    }
+    
+    protected function registerPublishes(): void
+    {
+        if (!$this->app->runningInConsole()) {
+            return;
+        }
+
+        $this->publishes([
+            __DIR__.'/../config/sentry_queue.php' => config_path('sentry_queue.php'),
+        ], 'config');
     }
 }
